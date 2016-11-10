31ab3a30daec5ec47bbdb620ea6e0efe12ff2a8e
liujidong <wwwliujidong@163.com>
2016-11-10 22:45:57 +0800

    js dom

--- lightbox.js
+++ lightbox.js
@@ -1,7 +1,34 @@
 ;(function($){
-	var lightBox = function(){
+		   
+	var LightBox = function(){
+		var self = this;
+		//创建遮罩和弹出窗
+		this.popupMask = $('<div id="G-lightbox-mask">');
+		this.popupWin = $('<div id="G-lightbox-popup">');
+		//保存body
+		this.bodyNode=$(document.body);
+		//渲染剩余的dom，并加入到body
+		this.renderDOM();
 	};
-	lightBox.prototype={
+	LightBox.prototype={
+		renderDOM:function(){
+			var strDom='<div class="lightbox-pic-view">'+
+							'<span class="lightbox-btn lightbox-prev-btn"></span>'+
+							'<img class="lightbox-image" src="images/2-2.jpg" width="100%">'+
+							'<span class="lightbox-btn lightbox-next-btn"></span>'+
+						'</div>'+
+						'<div class="lightbox-pic-caption">'+
+							'<div class="lightbox-caption-area">'+
+								'<p class="lightbox-pic-desc">图片标题</p>'+
+								'<span class="lightbox-of-index">当前索引：0 of 0 </span>'+
+							'</div>'+
+							'<span class="lightbox-close-btn"></span>'+
+						'</div>';
+			//插入到this.popupWin
+			this.popupWin.html(strDom);
+			//把遮罩和弹出框插入到body
+			this.bodyNode.append(this.popupMask,this.popupWin);
+			}
 	};		
-	window["lightBox"]=lightBox;
-	})(jQuery);
+	window["LightBox"]=LightBox;
+})(jQuery);
